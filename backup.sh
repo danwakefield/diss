8f6e9595570d47f4714a50f6f060fb74c4a4b5d0
Daniel Wakefield <dan@danw.eu>
2016-04-13 16:26:40 +0100

    Update backup script

--- backup.sh
+++ backup.sh
@@ -1,4 +1,10 @@
 cd ~/documents/diss
-git pull --rebase
-git push github master
-notify-send "Diss" "Backed Up"
+{
+git pull --rebase &>/dev/null
+git push github master &>/dev/null
+}
+if [ $? -eq 0 ];then
+    notify-send "Diss" "Backed Up"
+else
+    notify-send "Diss" "Backup FAILED"
+fi
